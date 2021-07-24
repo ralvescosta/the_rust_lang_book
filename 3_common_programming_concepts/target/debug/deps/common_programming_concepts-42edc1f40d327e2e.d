f082764e650ec3209bb15c0fecea2ecb3c8e2d85
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-24 08:58:13 -0300

    refactor: gitignore

--- 3_common_programming_concepts/target/debug/deps/common_programming_concepts-42edc1f40d327e2e.d
+++
@@ -1,5 +0,0 @@
-/home/ralvescosta/Desktop/Rafael/the_rust_lang_book/3_common_programming_concepts/target/debug/deps/common_programming_concepts-42edc1f40d327e2e.rmeta: src/main.rs
-
-/home/ralvescosta/Desktop/Rafael/the_rust_lang_book/3_common_programming_concepts/target/debug/deps/common_programming_concepts-42edc1f40d327e2e.d: src/main.rs
-
-src/main.rs:
--- 3_common_programming_concepts/target/debug/deps/common_programming_concepts-87a510a0052fa69c.d
+++
@@ -1,5 +0,0 @@
-/home/ralvescosta/Desktop/Rafael/the_rust_lang_book/3_common_programming_concepts/target/debug/deps/common_programming_concepts-87a510a0052fa69c.rmeta: src/main.rs
-
-/home/ralvescosta/Desktop/Rafael/the_rust_lang_book/3_common_programming_concepts/target/debug/deps/common_programming_concepts-87a510a0052fa69c.d: src/main.rs
-
-src/main.rs:
