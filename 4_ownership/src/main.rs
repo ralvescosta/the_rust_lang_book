7540387795d77deccaf9470a79586e861eea99d0
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-24 20:52:18 -0300

    feat: ownership

---
+++ 4_ownership/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // --- Ownership Rules ---
+
+    // 1.Each value in Rust has a variable, that's called its owner
+    // 2. There can only be one owner at a time
+    // 3. When the owner goes out of scop, the value will be dropped
+}
