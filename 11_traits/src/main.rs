a65508ed448e3354588058198c89e183b7aac0ed
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-27 08:58:50 -0300

    feat: traits

---
+++ 11_traits/src/main.rs
@@ -0,0 +1,58 @@
+use std::fmt::Display;
+
+pub trait Summary {
+    fn summarize(&self) -> String {
+        // default implementation will be overwrite when this trait is implemented
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {}", self.headline, self.author)
+    }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub replay: bool,
+    pub retweet: bool,
+}
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}, by {}", self.username, self.content)
+    }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize())
+}
+
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+}
+fn main() {
+    println!("Hello, world!");
+}
