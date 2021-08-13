0707d5d1a8f26700a9f788367cf44e8bae4614aa
ralvescosta <rafael.rac.mg@gmail.com>
2021-08-13 07:45:44 -0300

    smart pointers part 2

--- 23_smart_pointers_2/src/main.rs
+++ 23_smart_pointers_2/src/main.rs
@@ -1,3 +1,64 @@
+use std::ops::Deref;
 fn main() {
-    println!("Hello, world!");
+    first_example_wrong_assert();
+    second_example_with_box();
+    third_example_ref_coercion();
+}
+
+fn first_example_wrong_assert() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    //assert_eq(5, y);
+}
+
+fn second_example_with_box() {
+    struct MyBox<T>(T);
+    impl<T> MyBox<T> {
+        pub fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    };
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &T {
+            &self.0
+        }
+    };
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *(y.deref()));
+    assert_eq!(5, *y);
+}
+
+fn third_example_ref_coercion() {
+    struct MyBox<T>(T);
+    impl<T> MyBox<T> {
+        pub fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    };
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &T {
+            &self.0
+        }
+    };
+
+    fn hello(name: &str) {
+        println!("Hello, {}!", name);
+    }
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    hello(&(*m)[..]);
 }
