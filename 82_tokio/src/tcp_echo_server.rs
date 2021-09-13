1c81ed22e021c5b811905729bc87dd846e303a67
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-13 20:44:07 -0300

    refactor: send echo msgs to all clients

--- 82_tokio/src/tcp_echo_server.rs
+++ 82_tokio/src/tcp_echo_server.rs
@@ -1,13 +1,19 @@
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 pub async fn run() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel::<String>(10);
+
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
@@ -15,9 +21,21 @@ pub async fn run() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    },
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                };
             }
         });
     }
