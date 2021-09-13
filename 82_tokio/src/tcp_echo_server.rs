971e0920d2317c7d072b3cb1c50847e512a32cd1
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-13 13:39:46 -0300

    feat: tcp server could accept multiple connection but cant handle each mgs

--- 82_tokio/src/tcp_echo_server.rs
+++ 82_tokio/src/tcp_echo_server.rs
@@ -1,16 +1,22 @@
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
 };
 
 pub async fn run() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
     loop {
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let (reader, mut writer) = socket.split();
+        let mut reader = BufReader::new(reader);
+        let mut line = String::new();
+
+        loop {
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+            writer.write_all(line.as_bytes()).await.unwrap();
+            line.clear();
+        }
     }
 }
