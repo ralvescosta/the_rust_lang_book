a8457edca75eb271e24eef3f0e345d1db89baafd
ralvescosta <rafael.rac.mg@gmail.com>
2021-09-13 08:52:02 -0300

    takio basic

---
+++ 82_tokio/src/basic_tokio.rs
@@ -0,0 +1,22 @@
+pub async fn run() {
+    let result = tokio::join!(
+        tokio::spawn(async {
+            println!("1 - Started");
+            println!("1 - Do some jobs! - await 3s...");
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+            println!("1 - Finish my job");
+        }),
+        tokio::spawn(async {
+            println!("2 - Started");
+            println!("2 - Do some jobs! - await 3s...");
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+            println!("2 - Finish my job");
+        }),
+        tokio::spawn(async {
+            println!("3 - Started");
+            println!("3 - Do some jobs! - await 3s...");
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+            println!("3 - Finish my job");
+        })
+    );
+}
--- 82_tokio/src/main.rs
+++ 82_tokio/src/main.rs
@@ -1,27 +1,8 @@
 use tokio;
+mod basic_tokio;
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
-    let result = tokio::join!(
-        tokio::spawn(async {
-            println!("1 - Started");
-            println!("1 - Do some jobs! - await 3s...");
-            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
-            println!("1 - Finish my job");
-        }),
-        tokio::spawn(async {
-            println!("2 - Started");
-            println!("2 - Do some jobs! - await 3s...");
-            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
-            println!("2 - Finish my job");
-        }),
-        tokio::spawn(async {
-            println!("3 - Started");
-            println!("3 - Do some jobs! - await 3s...");
-            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
-            println!("3 - Finish my job");
-        })
-    );
-
+    basic_tokio::run().await;
     Ok(())
 }
